4e480478078bb7e45587f0b8aeafac56b4cb4ac6
xiaoyu <1765841705@qq.com>
2025-09-01 14:16:48 +0800

    feat(Flow): 初始化 FlowBuilder 类
    - 添加 FlowBuilder 类，实现审批流构建的基础功能
    - 新增 setType 方法，用于设置审批流类型
    - 添加 flow、node 和 task 方法，用于构建审批流- 引入 strict_types声明，提高代码类型安全性

--- app/Flow/FlowBuilder.php
+++ app/Flow/FlowBuilder.php
@@ -1,8 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Flow;
 
 /**
  * 负责创建审批流程
  */
-final class FlowBuilder {}
+final class FlowBuilder
+{
+    /** @var string 审批流类型 */
+    private string $type;
+
+    /**
+     * @param  string  $type
+     * @return void
+     */
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    public function flow(): self
+    {
+        return $this;
+    }
+
+    public function node(): self
+    {
+        return $this;
+    }
+
+    public function task(): self
+    {
+        return $this;
+    }
+}
